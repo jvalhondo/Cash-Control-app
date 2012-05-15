1e93399390d50cc6bd9f482c1a04b2edb0cbe16d
elvalh@gmail.com <elvalh@gmail.com@6fe84cc1-c2e5-f952-7bac-28a39c19954c>
2012-05-15 11:35:28 +0000

    Mejorada la parte visual de la edicion y creacion de notas, incluidos campos date y time.
    Falta que los nuevos campos se incluyan en la base de datos.
    Como seguir? quizas cambiar que en lugar de ser un ListView sea un Table... (sugerencia)
    Asegurarse que hay campos obligatorios y no se crean huecos en la base de datos.
    
    Resumen de lo que falta a corto plazo: gestion base de datos. Posibilidad de cambiar la presentacion de las entradas a una tabla y no una lista.

--- trunk/CashControl/src/swcm/android/NoteEdit.java
+++ trunk/CashControl/src/swcm/android/NoteEdit.java
@@ -1,17 +1,43 @@
 package swcm.android;
 
+import java.util.Calendar;
+
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 public class NoteEdit extends Activity {
 	private EditText mTitleText;
 	private EditText mDescriptionText;
 	private Long mRowId;
 	private NotesDbAdapter mDbHelper;
+	
+	//for the date button"Set Date"
+	private TextView mDateDisplay;
+    private Button mPickDate;
+    private int mYear;
+    private int mMonth;
+    private int mDay;
+
+    static final int DATE_DIALOG_ID = 0;
+    
+    //for the date button"Set Date"
+    private TextView mTimeDisplay;
+    private Button mPickTime;
+
+    private int mHour;
+    private int mMinute;
+
+    static final int TIME_DIALOG_ID = 1;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +51,42 @@ public class NoteEdit extends Activity {
 		
 		mTitleText = (EditText) findViewById(R.id.person);
 		mDescriptionText = (EditText) findViewById(R.id.description);
+		
+		// capture our View elements Set Date
+        mDateDisplay = (TextView) findViewById(R.id.dateDisplay);
+        mPickDate = (Button) findViewById(R.id.pickDate);
+        // add a click listener to the button
+        mPickDate.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                showDialog(DATE_DIALOG_ID);
+            }
+        });
+        // get the current date
+        final Calendar dateCalendar = Calendar.getInstance();
+        mYear = dateCalendar.get(Calendar.YEAR);
+        mMonth = dateCalendar.get(Calendar.MONTH);
+        mDay = dateCalendar.get(Calendar.DAY_OF_MONTH);
+
+        // display the current date (this method is below)
+        updateDateDisplay();
+        
+        // capture our View elements Set Time
+        mTimeDisplay = (TextView) findViewById(R.id.timeDisplay);
+        mPickTime = (Button) findViewById(R.id.pickTime);
+        // add a click listener to the button
+        mPickTime.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                showDialog(TIME_DIALOG_ID);
+            }
+        });
+        // get the current time
+        final Calendar timeCalendar = Calendar.getInstance();
+        mHour = timeCalendar.get(Calendar.HOUR_OF_DAY);
+        mMinute = timeCalendar.get(Calendar.MINUTE);
+
+        // display the current date
+        updateTimeDisplay();
+        
 		
 		Button confirmButton = (Button) findViewById(R.id.confirm);
 		
@@ -47,6 +109,44 @@ public class NoteEdit extends Activity {
 		    
 		});
 	}
+	// updates the date we display in the TextView
+	private void updateDateDisplay() {
+		mDateDisplay.setText(
+	            new StringBuilder()
+	                    // Month is 0 based so add 1
+	                    .append(mMonth + 1).append("-")
+	                    .append(mDay).append("-")
+	                    .append(mYear).append(" "));
+		
+	}
+	
+	// the callback received when the user "sets" the date in the dialog
+    private DatePickerDialog.OnDateSetListener mDateSetListener =
+            new DatePickerDialog.OnDateSetListener() {
+
+                public void onDateSet(DatePicker view, int year, 
+                                      int monthOfYear, int dayOfMonth) {
+                    mYear = year;
+                    mMonth = monthOfYear;
+                    mDay = dayOfMonth;
+                    updateDateDisplay();
+                }
+            };
+    
+ // updates the time we display in the TextView
+    private void updateTimeDisplay() {
+        mTimeDisplay.setText(
+            new StringBuilder()
+                    .append(pad(mHour)).append(":")
+                    .append(pad(mMinute)));
+    }
+    
+    private static String pad(int c) {
+        if (c >= 10)
+            return String.valueOf(c);
+        else
+            return "0" + String.valueOf(c);
+    }
 
 	private void populateFields() {
 		if (mRowId != null) {
@@ -58,6 +158,31 @@ public class NoteEdit extends Activity {
 	                note.getColumnIndexOrThrow(NotesDbAdapter.KEY_BODY)));
 	    }
 	}
+	
+	// the callback received when the user "sets" the time in the dialog
+	private TimePickerDialog.OnTimeSetListener mTimeSetListener =
+	    new TimePickerDialog.OnTimeSetListener() {
+	        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+	            mHour = hourOfDay;
+	            mMinute = minute;
+	            updateTimeDisplay();
+	        }
+	    };
+	
+	    @Override
+	    protected Dialog onCreateDialog(int id) {
+	    	switch (id) {
+	        case DATE_DIALOG_ID:
+	            return new DatePickerDialog(this,
+	                        mDateSetListener,
+	                        mYear, mMonth, mDay);
+	        case TIME_DIALOG_ID:
+	            return new TimePickerDialog(this,
+	                    mTimeSetListener, mHour, mMinute, false);
+	        }
+	        return null;
+	    }
+	    
 	
 	@Override
     protected void onSaveInstanceState(Bundle outState) {
