8ec9ad65d1934833e10dc2df7d55e62fcd651e12
Jorge Valhondo Rama <jvalhondo@users.noreply.github.com>
2012-05-17 18:41:38 +0000

    Introducidos los campos amount, date, time

--- CashControl/src/swcm/android/NotesDbAdapter.java
+++ CashControl/src/swcm/android/NotesDbAdapter.java
@@ -22,6 +22,9 @@ public class NotesDbAdapter {
 
     public static final String KEY_PERSON = "person";
     public static final String KEY_DESCRIPTION = "description";
+    public static final String KEY_AMOUNT = "amount";
+    public static final String KEY_DATE = "date";
+    public static final String KEY_TIME = "time";
     public static final String KEY_ROWID = "_id";
 
     private static final String TAG = "NotesDbAdapter";
@@ -33,7 +36,8 @@ public class NotesDbAdapter {
      */
     private static final String DATABASE_CREATE =
         "create table notes (_id integer primary key autoincrement, "
-        + "person text not null, description text not null);";
+        + "person text not null, description text not null, amount text not null,"
+        + "date text not null, time text not null);";
 
     private static final String DATABASE_NAME = "data";
     private static final String DATABASE_TABLE = "notes";
